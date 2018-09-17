7eee798226a3395125555a40fa40b528fa76a2a4
Phillip Swope <ps0895423@otc.edu>
2018-09-16 20:19:40 -0500

    Aurora is done

---
+++ app.js
@@ -0,0 +1,22 @@
+// TODO: create the character object here
+const aurora = {
+    name: "Aurora",
+    health: 150,
+    strength: 25,
+    xp: 0,
+    describe(){
+        return `${this.name} has ${this.health} health points, ${this.strength} as strength and ${this.xp} XP points`;
+}
+}
+// Aurora is harmed by an arrow
+aurora.health -= 20;
+
+// Aurora equips a strength necklace
+aurora.strength += 10;
+
+// Aurora learn a new skill
+aurora.xp += 15;
+
+console.log(aurora.describe());
+
+
